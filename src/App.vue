b12cc74d382e44d44ad3bbcfab4badb5e0b39943
sahad-cp <sahadmanu4@gmail.com>
2023-11-07 22:09:40 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <SingupForm/>
+</template>
+
+<script>
+import SingupForm from "./components/SingupForm.vue";
+
+export default {
+  components:{ SingupForm }
+}
+
+</script>
+
+<style>
+
+</style>
